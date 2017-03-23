474cf3eed98240d80421baa7c436888975204b3d
elenagarrone <elena.15@hotmail.it>
2017-03-23 11:09:11 +0000

    [EG] Add filtering options

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+$('.hero-filters').on('click', '.btn', function(){
+  var $el = $(this);
+  if ($el.hasClass('active')) {
+    $el.find('i').removeClass('fa-times').addClass('fa-plus');
+  } else {
+    $el.find('i').removeClass('fa-plus').addClass('fa-times');
+  }
+  $el.toggleClass('active');
+});
+
+$('#filter').on('click', function(){
+   $('.rm').css('display', 'none');
+   $('.filtered').css('display', 'block');
+});
